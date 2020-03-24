5a53cec01a45c979a646d04ef5db702b3a154e2b
Nic Schumann <nic@nicschumann.co>
2020-03-24 11:51:31 -0400

    [handles] adds handle angles, and conditional context toggler.

--- AllAngles.glyphsReporter/Contents/Resources/plugin.py
+++ AllAngles.glyphsReporter/Contents/Resources/plugin.py
@@ -19,6 +19,7 @@ from GlyphsApp import *
 from GlyphsApp.plugins import *
 
 LINE_COLOR=(56/256, 217/256, 137/256, 1)
+QUAD_COLOR=(217/256, 56/256, 107/256, 1)
 
 def get_normed_vector(x, y):
 	length = sqrt(x**2 + y**2)
@@ -46,34 +47,63 @@ def get_points_from_line(segment):
 
 class AllAngles(ReporterPlugin):
 
+	show_lines = True
+	show_quads = False
+
 	@objc.python_method
 	def settings(self):
 		self.menuName = Glyphs.localize({'en': u'All Angles'})
 		self.generalContextMenus = [
-			{'name': Glyphs.localize({'en': u'Show angles for all straight line segments on the layer.', 'de': u'Tu etwas'}) },
+			{'name': Glyphs.localize({'en': 'Hide Line Angles '}), 'action': self.toggle_lines},
+			{'name': Glyphs.localize({'en': 'Show Handle Angles'}), 'action': self.toggle_quads}
 		]
 
 	@objc.python_method
 	def foreground(self, layer):
 		for path in layer.paths:
-			for segment in filter(lambda s: len(s) == 2 , path.segments):
-				x1, y1, x2, y2 = get_points_from_line(segment)
-				angle = get_angle_from_points(x1, y1, x2, y2)
-				prettyAngle = u"%s°" % str(round(angle, 1))
+			for segment in path.segments:
+				if len(segment) == 2 and self.show_lines:
+					self.render_indicator_for_line_segment(segment, draw_color=LINE_COLOR)
+				elif len(segment) == 4 and self.show_quads:
+					p1, p2, p3, p4 = segment
+					quad_segment_1 = (p1, p2)
+					quad_segment_2 = (p3, p4)
+					self.render_indicator_for_line_segment(quad_segment_1, draw_color=QUAD_COLOR)
+					self.render_indicator_for_line_segment(quad_segment_2, draw_color=QUAD_COLOR)
 
-				offset_scale = 14/self.getScale()
-				x_mid, y_mid = get_intermediate_from_points(x1, y1, x2, y2)
-				x_norm, y_norm = get_normed_vector(x2 - x1, y2 - y1)
-				x_orth, y_orth = get_rotated_vector(x_norm, y_norm)
-				x_mid_offset, y_mid_offset = x_mid+offset_scale*x_orth, y_mid+offset_scale*y_orth
 
-				color = NSColor.colorWithCalibratedRed_green_blue_alpha_(*LINE_COLOR)
+	def render_indicator_for_line_segment(self, segment, draw_color=LINE_COLOR):
+		x1, y1, x2, y2 = get_points_from_line(segment)
+		angle = get_angle_from_points(x1, y1, x2, y2)
+		prettyAngle = u"%s°" % str(round(angle, 1))
 
-				x_text_anchor, y_text_anchor = self.get_text_anchor(prettyAngle, x_mid, y_mid, x_mid_offset, y_mid_offset)
+		offset_scale = 14/self.getScale()
+		x_mid, y_mid = get_intermediate_from_points(x1, y1, x2, y2)
+		x_norm, y_norm = get_normed_vector(x2 - x1, y2 - y1)
+		x_orth, y_orth = get_rotated_vector(x_norm, y_norm)
+		x_mid_offset, y_mid_offset = x_mid+offset_scale*x_orth, y_mid+offset_scale*y_orth
 
-				color.set()
-				self.draw_indicator((x_mid, y_mid), (x_mid_offset, y_mid_offset))
-				self.drawTextAtPoint(prettyAngle, NSPoint(x_text_anchor, y_text_anchor), fontColor=color )
+		color = NSColor.colorWithCalibratedRed_green_blue_alpha_(*draw_color)
+
+		x_text_anchor, y_text_anchor = self.get_text_anchor(prettyAngle, x_mid, y_mid, x_mid_offset, y_mid_offset)
+
+		color.set()
+		self.draw_indicator((x_mid, y_mid), (x_mid_offset, y_mid_offset))
+		self.drawTextAtPoint(prettyAngle, NSPoint(x_text_anchor, y_text_anchor), fontColor=color )
+
+
+	@objc.python_method
+	def toggle_lines(self):
+		self.show_lines = not self.show_lines
+		menuName = 'Hide Line Angles' if self.show_lines else 'Show Line Angles'
+		self.generalContextMenus[0] = {'name': Glyphs.localize({'en': menuName}), 'action': self.toggle_lines}
+
+
+	@objc.python_method
+	def toggle_quads(self):
+		self.show_quads = not self.show_quads
+		menuName = 'Hide Handle Angles' if self.show_quads else 'Show Handle Angles'
+		self.generalContextMenus[1] = {'name': Glyphs.localize({'en': menuName}), 'action': self.toggle_lines}
 
 
 	@objc.python_method
